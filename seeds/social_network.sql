9d77ba13fcfd40161196d1927a8a31c133891773
Zuhair <khanzuhair96@live.co.uk>
2025-06-25 18:17:17 +0100

    fixed bug with duplicate tables

--- seeds/social_network.sql
+++ seeds/social_network.sql
@@ -1,9 +1,11 @@
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email_address text,
     username text
 );
-
 
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
@@ -16,5 +18,5 @@ CREATE TABLE posts (
         on delete cascade
 );
 
-INSERT INTO users (email_address, username) VALUES ("johndo@example.com", "john_do");
-INSERT INTO users (email_address, username) VALUES ("jamiedo@example.com", "jamie_do");
+INSERT INTO users (email_address, username) VALUES ('johndo@example.com', 'john_do');
+INSERT INTO users (email_address, username) VALUES ('jamiedo@example.com', 'jamie_do');
